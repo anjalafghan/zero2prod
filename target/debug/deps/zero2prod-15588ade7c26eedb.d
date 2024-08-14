8ad96a086dc5335c55b25f6a5c6b39a30cf06723
Anjal Afghan <anjal.afghan93@gmail.com>
2024-08-14 21:22:39 +0530

    project skeleton

---
+++ target/debug/deps/zero2prod-15588ade7c26eedb.d
@@ -0,0 +1,5 @@
+/Users/anjalafghan/Documents/rust/zero2prod/target/debug/deps/libzero2prod-15588ade7c26eedb.rmeta: src/main.rs
+
+/Users/anjalafghan/Documents/rust/zero2prod/target/debug/deps/zero2prod-15588ade7c26eedb.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/zero2prod-9f0f3d2cb2d43d2a.d
@@ -0,0 +1,5 @@
+/Users/anjalafghan/Documents/rust/zero2prod/target/debug/deps/libzero2prod-9f0f3d2cb2d43d2a.rmeta: src/main.rs
+
+/Users/anjalafghan/Documents/rust/zero2prod/target/debug/deps/zero2prod-9f0f3d2cb2d43d2a.d: src/main.rs
+
+src/main.rs:
