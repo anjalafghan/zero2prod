cef1e742c8ab35271c2c8547e37af449367074e1
Anjal Afghan <anjal.afghan93@gmail.com>
2026-01-03 15:41:59 +0530

    unit test works

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,6 @@
-use actix_web::{App, HttpResponse, HttpServer, Responder, get, post, web};
-
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok().finish()
-}
+use zero2prod::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
-            .route("/health_check", web::get().to(health_check))
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    run()?.await
 }
